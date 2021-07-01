63ab7c6ab268e9b928ea0df143913831b8f3502e
dezfuture <agrim2000@gmail.com>
2021-07-01 16:14:17 +0530

    -900

--- @900/A_Strange_Partition.cpp
+++ @900/A_Strange_Partition.cpp
@@ -1,28 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(0);
-#define endl "\n"
-#define ull unsigned long long
-#define pb push_back
-typedef long long ll;
-typedef pair<int, int> pii;
-const int MOD = 1000000007;
-const int N = 1e3 + 2;
-const int MAXN = 2e5 + 5;
-
-int32_t main()
+void fast()
 {
-    fast_io;
-
-    int t;
-    cin >> t;
-    while (t--)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define res(i, a, b) for (ll i = a; i >= b; i--)
+#define all(n) n.begin(), n.end()
+#define mod 1000000007
+template <typename T>
+void pop_front(std::vector<T> &vec)
+{
+    assert(!vec.empty());
+    vec.erase(vec.begin());
+}
+typedef long long ll;
+const long long INF = 1e18 + 42;
+bool comp(pair<ll, ll> v1, pair<ll, ll> v2)
+{
+    return v1.first < v2.first;
+}
+void sol()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> v1(n);
+    ll minval = 0, maxval = 0, sum = 0;
+    rep(i, 0, n)
     {
-        solve();
+        cin >> v1[i];
+        sum += v1[i];
+        if (v1[i] % m == 0)
+        {
+            maxval += v1[i] / m;
+        }
+        else
+        {
+            maxval += (v1[i] / m) + 1;
+        }
     }
 
-    return 0;
+    if (sum % m == 0)
+    {
+        minval += sum / m;
+    }
+    else
+    {
+        minval += (sum / m) + 1;
+    }
+    cout << minval << " " << maxval << endl;
+}
+int main()
+{
+    fast();
+    int a = 1;
+    cin >> a;
+    while (a--)
+    {
+        sol();
+    }
 }
