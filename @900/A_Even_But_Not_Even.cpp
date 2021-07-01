0ae5aa3b7d4a9cc35891186c2abc247098c01c8d
dezfuture <agrim2000@gmail.com>
2021-07-02 00:26:12 +0530

    ->900

--- @900/A_Even_But_Not_Even.cpp
+++ @900/A_Even_But_Not_Even.cpp
@@ -13,31 +13,10 @@ const int MOD = 1000000007;
 const int N = 1e3 + 2;
 const int MAXN = 2e5 + 5;
 
-int calc(int a, int b, int c)
-{
-    return abs(a - b) + abs(b - c) + abs(c - a);
-}
-
 void solve()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-
-    int res = calc(a, b, c);
-    for (int x = -1; x <= 1; x++)
-    {
-        for (int y = -1; y <= 1; y++)
-        {
-            for (int z = -1; z <= 1; z++)
-            {
-                int a1 = a + x;
-                int b1 = b + y;
-                int c1 = c + z;
-                res = std::min(res, calc(a1, b1, c1));
-            }
-        }
-    }
-    cout << res << endl;
+    ll n, number;
+    cin >> n >> number;
 }
 
 int32_t main()
