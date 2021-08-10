eb1592fa450b727db6e3f0317f229f5a02f98705
dezfuture <agrim2000@gmail.com>
2021-08-11 02:12:36 +0530

    @1100

---
+++ @1100/C_Sum_of_Cubes.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(0);
+#define endl "\n"
+#define ull unsigned long long
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+const int MOD = 1000000007;
+const int N = 1e3 + 2;
+const int MAXN = 2e5 + 5;
+
+const int M = 1e9 + 7;
+long long mod(long long x)
+{
+    return ((x % M + M) % M);
+}
+long long add(long long a, long long b)
+{
+    return mod(mod(a) + mod(b));
+}
+long long mul(long long a, long long b)
+{
+    return mod(mod(a) * mod(b));
+}
+
+ll modPow(ll a, ll b)
+{
+    if (b == 0)
+        return 1LL;
+    if (b == 1)
+        return a % M;
+    ll res = 1;
+    while (b)
+    {
+        if (b % 2 == 1)
+            res = mul(res, a);
+        a = mul(a, a);
+        b = b / 2;
+    }
+    return res;
+}
+
+void sieve(int n, bool isPrime[])
+{
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+int lcm(int a, int b)
+{
+    int c = __gcd(a, b);
+    return (a / c) * b;
+}
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    ll a = 1, b = 1e4;
+    string res = "NO";
+    while (a <= b)
+    {
+        ll sum = (a * a * a) + (b * b * b);
+        if (sum == x)
+        {
+            res = "YES";
+            break;
+        }
+        else if (sum < x)
+        {
+            a++;
+        }
+        else
+        {
+            b--;
+        }
+    }
+
+    cout << res << endl;
+}
+
+int32_t main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
